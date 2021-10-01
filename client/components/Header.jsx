c45d6918912370443c983a16198d8681866500b1
Carter-Bardell-Munro <cartermunro11@gmail.com>
2021-10-01 15:09:13 +1300

    logo now links to home page

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,20 +1,15 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Nav from './Nav'
 
 function Header () {
   return (
     <header>
-      <img src='images/ghibli-logo.png' className="logo-image" />
+      <Link to='/'>
+        <img src='images/ghibli-logo.png' className="logo-image" />
+      </Link>
       <Nav />
     </header>
-
-  // <div className="container">
-  //   {/* <img src="https://wallpapercave.com/wp/aWaXW72.jpg" /> */}
-  //   <div className="nav-header">
-  //     <h1>The Studio Ghibli Collection</h1>
-  //   </div>
-  // </div>
   )
 }
 
